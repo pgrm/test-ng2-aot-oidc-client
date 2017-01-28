b4a6b2724ea514bf2a3c43257a48d52988c8f763
Peter Grman <peter.grman@gmail.com>
2017-01-28 17:27:54 +0100

    add oidc client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { UserManager, WebStorageStateStore } from 'oidc-client';
+import { FactoryProvider } from '@angular/core';
+
 import { AppComponent } from './app.component';
+
+const settings = { /* ... */ };
+
+const userManager = new UserManager(settings);
+
+export function getUserManager() {
+  return userManager;
+}
+
+export const UserManagerProvider: FactoryProvider = {
+  provide: UserManager,
+  useFactory: getUserManager
+};
+
 
 @NgModule({
   declarations: [
@@ -14,7 +31,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [UserManagerProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
